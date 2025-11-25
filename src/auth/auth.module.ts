58dc62b308ce9117af441027c78e6d723855e8d4
Caio Viana <caiodeoliveira076@gmail.com>
2025-11-25 16:47:21 -0300

    editar

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,6 @@ import { UserModule } from 'src/user/user.module';
 import { CommonModule } from 'src/common/common.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
-
 @Module({
   imports: [
     UserModule,
